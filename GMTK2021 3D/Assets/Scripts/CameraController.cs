463123223a38c2e193b919e455ede7a71f22b2f3
Brian Burns <brian.b5829@gmail.com>
2021-06-12 10:49:13 +1000

    created Stalwart. player has stun ability

--- GMTK2021 3D/Assets/Scripts/CameraController.cs
+++ GMTK2021 3D/Assets/Scripts/CameraController.cs
@@ -7,6 +7,8 @@ public class CameraController : MonoBehaviour
     public GameObject target;
     public Vector3 offset;
 
+    public GameObject blocking;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +19,25 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         transform.position = target.transform.position + offset;
+
+        Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.layer == 7)
+            {
+                blocking = hit.collider.gameObject;
+                blocking.GetComponent<MeshRenderer>().enabled = false;
+            }
+            else
+            {
+                if (blocking != null)
+                {
+                    blocking.GetComponent<MeshRenderer>().enabled = true;
+                    blocking = null;
+                }
+               
+            }
+        }
     }
 }
