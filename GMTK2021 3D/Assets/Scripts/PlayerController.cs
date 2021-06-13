4fb693925575af2f3fce36dc3ebc4bb8fc0f8da4
Brian Burns <brian.b5829@gmail.com>
2021-06-13 13:32:12 +1000

    added arm part, which allows you to move movable objects. added moveable box

--- GMTK2021 3D/Assets/Scripts/PlayerController.cs
+++ GMTK2021 3D/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public enum PartSlot
     LeftArm,
     RightLeg,
     RightArm,
-    Body,
+    Bottom,
     None
 }
 
@@ -23,8 +23,10 @@ public class PlayerController : MonoBehaviour
     public Stun stun;
     private StepHelper stepHelper;
     public ShaderPointTracker colorShader;
-    private float yLimit;
     private float inputDisabledTimer;
+    private Vector2 fakefacing;
+
+    private GameObject movableObject;
 
     private Dictionary<PartSlot, GameObject> parts = new Dictionary<PartSlot, GameObject>();
     public List<Vector3> partPositions;
@@ -35,7 +37,6 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         stepHelper = GetComponent<StepHelper>();
         stepHelper.enabled = false;
-        yLimit = 1.5f;
     }
 
     public void Hit(int damage)
@@ -46,6 +47,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        fakefacing = Vector2.Perpendicular(new Vector2(-rb.velocity.x, -rb.velocity.z));
         inputDisabledTimer -= Time.deltaTime;
 
         colorShader.RadiusIndex = parts.Count;
@@ -72,15 +74,22 @@ public class PlayerController : MonoBehaviour
             EjectPart(part);
         }
 
+        //if either slot has a leg, the character will stay upright and face the direction of movement
         if (parts.ContainsKey(PartSlot.LeftLeg) || parts.ContainsKey(PartSlot.RightLeg))
         {
             Stabilize();
             FaceMovementDirection();
         }
+
+        if (parts.ContainsKey(PartSlot.RightArm) || parts.ContainsKey(PartSlot.LeftArm))
+        {
+            Pull();
+        }
+
+        //if both slots have a leg, the character will hop up steps with ease
         if (parts.ContainsKey(PartSlot.LeftLeg) && parts.ContainsKey(PartSlot.RightLeg))
         {
             stepHelper.enabled = true;
-            yLimit = transform.position.y + 0.2f;
         }
         else
         {
@@ -103,11 +112,32 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void Pull()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            if (movableObject != null)
+            {
+                movableObject.GetComponent<Rigidbody>().mass = 10;
+                FixedJoint j = movableObject.AddComponent<FixedJoint>();
+                j.connectedBody = rb;
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            if (movableObject != null)
+            {
+                movableObject.GetComponent<Rigidbody>().mass = 1000000;
+                Destroy(movableObject.GetComponent<FixedJoint>());
+            }
+        }
+    }
+
     private void FaceMovementDirection()
     {
         if ((rb.velocity.x > 0.1 || rb.velocity.x < -0.1 || rb.velocity.z > 0.1 || rb.velocity.z < -0.1))
         {
-            Vector3 fakefacing = Vector2.Perpendicular(new Vector2(-rb.velocity.x, -rb.velocity.z));
+            
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(new Vector3(fakefacing.x, 0, fakefacing.y)), 2f);
         }
         
@@ -118,27 +148,78 @@ public class PlayerController : MonoBehaviour
         Debug.Log("adding part");
         if (g.CompareTag("Leg"))
         {
-            if (!parts.ContainsKey(PartSlot.LeftLeg))
+            if (SlotFree(0))
             {
-                AddLeg(g, PartSlot.LeftLeg);
+                AddPart(g, PartSlot.LeftLeg);
 
             }
-            else if (!parts.ContainsKey(PartSlot.RightLeg))
+            else if (SlotFree(1))
             {
-                AddLeg(g, PartSlot.RightLeg);
+                AddPart(g, PartSlot.RightLeg);
             }
             else
             {
-                Debug.Log("No more leg slots");
+                Debug.Log("No slot available");
+            }
+        }
+        else if (g.CompareTag("Arm"))
+        {
+            if (SlotFree(0))
+            {
+                AddPart(g, PartSlot.LeftArm);
+            }
+            else if (SlotFree(1))
+            {
+                AddPart(g, PartSlot.RightArm);
+            }
+            else
+            {
+                Debug.Log("No slot available");
             }
         }
     }
 
-    public void AddLeg(GameObject g, PartSlot slot)
+    //the index is as follows 0=left, 1=right, 2=bottom
+    private bool SlotFree(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                if (!parts.ContainsKey(PartSlot.LeftLeg) && !parts.ContainsKey(PartSlot.LeftArm))
+                {
+                    return true;
+                }
+                break;
+            case 1:
+                if (!parts.ContainsKey(PartSlot.RightLeg) && !parts.ContainsKey(PartSlot.RightArm))
+                {
+                    return true;
+                }
+                break;
+            case 2:
+                if (!parts.ContainsKey(PartSlot.Bottom))
+                {
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
+
+    public void AddPart(GameObject g, PartSlot slot)
     {
         parts[slot] = g;
         g.transform.SetParent(transform);
-        g.transform.rotation = new Quaternion(0, 0, 0, 0);
+        if (slot == PartSlot.LeftArm || slot == PartSlot.RightArm)
+        {
+            g.transform.rotation = Quaternion.LookRotation(new Vector3(fakefacing.x, 0, fakefacing.y));
+        }
+        else
+        {
+            g.transform.rotation = new Quaternion(0, 0, 0, 0);
+
+        }
+        
         g.transform.localPosition = partPositions[(int)slot];
         FixedJoint j = g.AddComponent<FixedJoint>();
         j.anchor = partPositions[(int)slot];
@@ -150,43 +231,59 @@ public class PlayerController : MonoBehaviour
         Debug.Log("yeet");
         if (g != null)
         {
-            if (g.CompareTag("Leg"))
+            Vector3 position = new Vector3();
+            Plane plane = new Plane(Vector3.up, 0);
+            plane.SetNormalAndPosition(Vector3.up, new Vector3(0, transform.position.y, 0));
+
+            float distance;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (plane.Raycast(ray, out distance))
             {
-                Vector3 position = new Vector3();
-                Plane plane = new Plane(Vector3.up, 0);
-                plane.SetNormalAndPosition(Vector3.up, new Vector3(0, transform.position.y, 0));
-
-                float distance;
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (plane.Raycast(ray, out distance))
-                {
-                    position = ray.GetPoint(distance);
-                }
-
-                g.transform.SetParent(null);
-                Destroy(g.GetComponent<FixedJoint>());
-                Part p = g.GetComponent<Part>();
-                p.interaction.gameObject.SetActive(true);
-                p.charged = true;
-                Vector3 force = Vector3.Normalize(new Vector3(position.x - transform.position.x, transform.position.y, position.z - transform.position.z));
-                g.GetComponent<Rigidbody>().AddForce(force * 10, ForceMode.Impulse);
-                rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
-                force = new Vector3(force.x, -0.2f, force.z);
-                rb.AddForce(-force * 5, ForceMode.Impulse);
-                DisableInput(0.5f);
-
-                //Remove the key,value that held the object 
-                PartSlot key = PartSlot.None;
-                foreach (KeyValuePair<PartSlot, GameObject> kv in parts)
-                {
-                    if (kv.Value == g)
-                    {
-                        key = kv.Key;
-                    }
-                }
-                parts.Remove(key);
+                position = ray.GetPoint(distance);
             }
+            g.transform.SetParent(null);
+            Destroy(g.GetComponent<FixedJoint>());
+            Part p = g.GetComponent<Part>();
+            p.interaction.gameObject.SetActive(true);
+            p.charged = true;
+            Vector3 force = Vector3.Normalize(new Vector3(position.x - transform.position.x, transform.position.y, position.z - transform.position.z));
+            g.GetComponent<Rigidbody>().AddForce(force * 10, ForceMode.Impulse);
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            force = new Vector3(force.x, -0.2f, force.z);
+            rb.AddForce(-force * 5, ForceMode.Impulse);
+            DisableInput(0.5f);
+
+            //Remove the key,value that held the object 
+            PartSlot key = PartSlot.None;
+            foreach (KeyValuePair<PartSlot, GameObject> kv in parts)
+            {
+                if (kv.Value == g)
+                {
+                    key = kv.Key;
+                }
+            }
+            parts.Remove(key);
         }
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Movable"))
+        {
+            movableObject = collision.gameObject;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Movable"))
+        {
+            if (movableObject == collision.gameObject)
+            {
+                Destroy(collision.gameObject.GetComponent<FixedJoint>());
+                movableObject = null;
+            }
+        }
     }
 }
